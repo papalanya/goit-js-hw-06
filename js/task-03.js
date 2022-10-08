0af7d0d87fe8972801182984ce66e267d95ce6c9
Illya Pavlenko <illiampavlenko@gmail.com>
2022-10-09 00:35:01 +0300

    add task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,18 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. 
+// Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
+const galleryImages = document.querySelector('.gallery');
+
+const newImages = images.map(({url, alt}) => `<li class='gallery__item'><img class='gallery__item--image' src='${url}' alt='${alt}' width = '350'></li>`
+);
+
+galleryImages.insertAdjacentHTML('beforebegin', newImages);
+
+galleryImages.style.display = "flex";
