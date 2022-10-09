30bea44655a31f5632d638a3d60b41cd53b14a6a
Illya Pavlenko <illiampavlenko@gmail.com>
2022-10-09 19:33:51 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Напиши скрипт, который изменяет цвета фона элемента <body>
+// через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+const toggle = document.querySelector('.change-color');
+const colorText = document.querySelector('.color');
+
+toggle.addEventListener('click', () => {
+  colorText.textContent = getRandomHexColor();
+  document.body.setAttribute('style', 'background-color: ' + colorText.textContent
+  );
+});
